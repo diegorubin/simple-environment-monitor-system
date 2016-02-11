5a31b453420a40319ae065e9e8d1b1c77a90d6c3
Diego Rubin <diegorubin@ciandt.com>
2016-02-11 12:50:37 -0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "sems",
-    version = "0.0.3",
-    url = "http://github.com/diegorubin/simple-environment-monitor-system",
-    author = "Diego Rubin",
-    author_email = "rubin.diego@gmail.com",
-    license = "",
+    name="sems",
+    version="0.0.3",
+    url="http://github.com/diegorubin/simple-environment-monitor-system",
+    author="Diego Rubin",
+    author_email="rubin.diego@gmail.com",
+    license="GPL2",
     scripts=['bin/sems-start'],
-    include_package_data= True,
-    install_requires = [
+    include_package_data=True,
+    description="A Simple Environment Monitor System",
+    install_requires=[
         "tornado",
         "tornado_json",
         "tinydb"
     ],
-    packages = find_packages()
+    classifiers=["'Development Status :: 3 - Alpha'"],
+    packages=find_packages()
 )
 
