d3585ef7493a0fae3825f51b6e3478005f1ad06f
Diego Rubin <diegorubin@ciandt.com>
2016-02-11 14:57:55 -0200

    update version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sems",
-    version="0.0.4",
+    version="0.0.5",
     url="http://github.com/diegorubin/simple-environment-monitor-system",
     author="Diego Rubin",
     author_email="rubin.diego@gmail.com",
