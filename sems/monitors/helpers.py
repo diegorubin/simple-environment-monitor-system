1b6ba544f3f759a7487f673ed470e110bcff5ad6
Diego Rubin <diegorubin@ciandt.com>
2018-08-23 09:18:50 -0300

    reading external monitors.

--- sems/monitors/helpers.py
+++ sems/monitors/helpers.py
@@ -7,7 +7,10 @@ except ImportError:
     import urllib2 as error
 
 from sems.monitors import *
+from sems.settings import TINY_DB_PATH
 from sems.utils import camel_to_snake_case
+
+external_monitors = []
 
 def load_monitors():
     for f in os.listdir(os.path.join(os.path.dirname(__file__))):
@@ -16,8 +19,22 @@ def load_monitors():
             if module not in sys.modules:
                 __import__(module)
 
+    external_monitor_path = os.path.join(os.path.dirname(TINY_DB_PATH))
+    sys.path.append(external_monitor_path)
+    for f in os.listdir(os.path.join(external_monitor_path, 'external')):
+        if(f.endswith("_monitor.py")):
+            monitor = f.replace('.py', '')
+            external_monitors.append(monitor)
+            module = "external.%s" % (monitor)
+            if module not in sys.modules:
+                __import__(module)
+
 def check_alive(monitor_type, url, **data):
-    module = "sems.monitors.%s" % (camel_to_snake_case(monitor_type))
+    monitor_type_module = camel_to_snake_case(monitor_type)
+    if monitor_type_module in external_monitors:
+        module = "external.%s" % (monitor_type_module)
+    else:
+        module = "sems.monitors.%s" % (monitor_type_module)
 
     if module not in sys.modules:
         __import__(module)
